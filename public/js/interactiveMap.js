301fba83bcb1c48282579804dd43cf06a82d1512
rubiak <adam334123@gmail.com>
2021-11-02 02:40:23 +0800

    map!!! :)

---
+++ public/js/interactiveMap.js
@@ -0,0 +1,61 @@
+var SINGAPORECOORDINATES = [1.352083, 103.819836]
+var USCOORDINATES = [37.09024, -95.712891]
+var TAIWANCOORDINATES = [23.69781, 120.960515]
+var JAPANCOORDINATES = [36.204824, 138.252924]
+var MALAYSIACOORDINATES = [4.210484, 101.975766]
+
+
+
+
+var mymap = L.map('map').setView(SINGAPORECOORDINATES, 2);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoid2FkMnByb2plY3QiLCJhIjoiY2t2Z3lyaXdvNHV3MTJwbXMxZ2R2eGw3NSJ9.Na27hF4TgK_PtBxdmSYMnw' //adam's token
+}).addTo(mymap);
+
+
+
+var BottleIcon = L.icon({
+    iconUrl: 'img/bottle.png',
+    shadowUrl: 'img/bottle_shadow.png',
+
+    iconSize:     [70, 35], // size of the icon
+    shadowSize:   [0, 0], // size of the shadow
+    iconAnchor:   [35, 18], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+});
+
+
+function addMarker(coordinates, header, description){
+    var marker = L.marker(coordinates, {icon: BottleIcon}).addTo(mymap);
+    marker.bindPopup(`<b>${header}</b><br>${description}`).openPopup();
+}
+addMarker(SINGAPORECOORDINATES, "林俊杰 JJ Lin", "Singapore");
+addMarker(USCOORDINATES, "Joe", "United States");
+addMarker(TAIWANCOORDINATES, "周杰伦 Jay Chou", "Taiwan");
+addMarker(JAPANCOORDINATES, "Piplup", "Japan")
+addMarker(MALAYSIACOORDINATES, "Namewee", "Malaysia")
+
+
+
+
+//click coordinates
+/**
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+}
+
+mymap.on('click', onMapClick);
+*/
