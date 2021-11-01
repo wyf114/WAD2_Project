8b30fad09a408eb86243606bd21e82ec29396adf
rubiak <adam334123@gmail.com>
2021-11-02 03:14:32 +0800

    map interactivity more smooth

--- public/js/interactiveMap.js
+++ public/js/interactiveMap.js
@@ -7,7 +7,17 @@ var MALAYSIACOORDINATES = [4.210484, 101.975766]
 
 
 
-var mymap = L.map('map').setView([45, 10], 2);
+var mymap = L.map('map', {
+    worldCopyJump: true,
+    minZoom: 1.5,
+    maxBounds: [
+    //south west
+    [-200, -200],
+    //north east
+    [200, 200]
+    ], 
+    maxBoundsViscosity: 1.0   
+    }).setView([45, 10], 2);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -15,7 +25,13 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'pk.eyJ1Ijoid2FkMnByb2plY3QiLCJhIjoiY2t2Z3lyaXdvNHV3MTJwbXMxZ2R2eGw3NSJ9.Na27hF4TgK_PtBxdmSYMnw' //adam's token
+
+    // this map option disables world wrapping. by default, it is false.
+    continuousWorld: false,
+
+    
+
+    accessToken: 'pk.eyJ1Ijoid2FkMnByb2plY3QiLCJhIjoiY2t2Z3lyaXdvNHV3MTJwbXMxZ2R2eGw3NSJ9.Na27hF4TgK_PtBxdmSYMnw', //adam's token
 }).addTo(mymap);
 
 
