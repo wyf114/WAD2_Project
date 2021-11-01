df4e2f5019991aa734d0121248567d74cf7ab50b
rubiak <adam334123@gmail.com>
2021-11-02 02:44:01 +0800

    map!! :)

--- public/js/interactiveMap.js
+++ public/js/interactiveMap.js
@@ -7,7 +7,7 @@ var MALAYSIACOORDINATES = [4.210484, 101.975766]
 
 
 
-var mymap = L.map('map').setView(SINGAPORECOORDINATES, 2);
+var mymap = L.map('map').setView([45, 10], 2);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
