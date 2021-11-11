b66a4575678b32d888eee5d6d77ea0ea75f602c1
piplupOwo <adam334123@gmail.com>
2021-11-11 23:16:01 +0800

    navbar with inbox

--- public/js/interactiveMap.js
+++ public/js/interactiveMap.js
@@ -1,16 +1,6 @@
 var username = sessionStorage.getItem("login_status");
 console.log(username);
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDLM5eiv5cOkBtM-os59uGPAhtFkdBCAhY",
-    authDomain: "sademo2018-2-11ec2.firebaseapp.com",
-    databaseURL: "https://sademo2018-2-11ec2.firebaseio.com",
-    projectId: "sademo2018-2-11ec2",
-    storageBucket: "sademo2018-2-11ec2.appspot.com",
-    messagingSenderId: "431393859582",
-    appId: "1:431393859582:web:ccc0434dd68846b15b82af"
-};
-firebase.initializeApp(firebaseConfig);
 
 
 
---
+++ public/js/navBar.js
@@ -0,0 +1,89 @@
+var username = sessionStorage.getItem("login_status");
+console.log(username);
+
+if (username === null){
+    window.location.replace("index.html")
+}
+
+
+
+var myComponent = Vue.extend({
+    data: function () {
+        return { messages: 0 }
+    },
+
+    template: `
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark" style="position: sticky;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <!-- right navbar links -->
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a id="navbar" class="nav-link active" aria-current="page" href="catchbottle.html">Bottle</a>
+                    </li>
+                    <li class="nav-item">
+                        <a id="navbar" class="nav-link" href="sendBottle.html">Send Bottle</a>
+                    </li>
+                    <li class="nav-item">
+                        <a id="navbar" class="nav-link" href="mapPage.html">Map Feature</a>
+                    </li>
+                    <li class="nav-item">
+                        <a id="navbar" class="nav-link" href="aboutUs.html">About Us</a>
+                    </li>
+                </ul>
+
+                <!-- left navbar links -->
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a id="navbar" class="nav-link" href="profile.html">Profile</a>
+                    </li>
+                    <li class="nav-item" style="height: 40px">
+                        <a id="navbar" class="nav-link" href="myMessage.html">Inbox</a>
+                        <span class="badge" id="notiNum">{{ messages }}</span>
+                    </li>
+                    <li class="nav-item">
+                        <a id="navbar" class="nav-link nav-logout" onclick="logout()">Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+            `,
+    methods: {
+        updateInbox() {
+        loggedUserMessagesInfo = firebase.database().ref(`messages/${username}`);
+        loggedUserMessagesInfo.once('value').then((snapshot) => {
+            if (snapshot.exists()) {
+                console.log(Object.keys(snapshot.val()).length);
+                const number_of_messages = Object.keys(snapshot.val()).length;
+
+                this.messages = number_of_messages;
+                console.log(number_of_messages);        
+            }
+        })
+        }
+    },
+    beforeMount(){
+        this.updateInbox();
+     },
+})
+
+Vue.component("nav-bar", myComponent);
+
+navapp = new Vue({
+    el: "#navapp",
+    data: {
+        messages: 2
+    }
+})
+
+
+
+
